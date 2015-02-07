08d9bc49188b1948464f83d14869ceac1bdd7966
Nathan <nrtheprogrammer@gmail.com>
2015-02-06 22:08:15 -0800

    Fixed broken imports.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 
-import net.minecraft.server.v1_7_R4.ChatSerializer;
-import net.minecraft.server.v1_7_R4.IChatBaseComponent;
-import net.minecraft.server.v1_7_R4.PacketPlayOutChat;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
