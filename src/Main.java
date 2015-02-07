fb0a70c26ef7d83d02a4b8538137a3761683fbdd
152cooperR <nrtheprogrammer@gmail.com>
2015-02-06 21:59:17 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+
+import net.minecraft.server.v1_7_R4.ChatSerializer;
+import net.minecraft.server.v1_7_R4.IChatBaseComponent;
+import net.minecraft.server.v1_7_R4.PacketPlayOutChat;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by natha_000 on 2/6/2015.
+ */
+public class Main extends JavaPlugin implements Listener {
+
+    public final Logger logger = Logger.getLogger("Minecraft");
+
+    public void onEnable(){
+
+        logger.info("BukkitTesting has been enabled!");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        Player p = (Player) sender;
+        if(commandLabel.equalsIgnoreCase("test")){
+            
+        }
+        return false;
+    }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+
+    }
+}
